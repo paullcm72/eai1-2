28277c9a5929023824eaf10153860a9ce5d21c86
paullcm72 <72821308+paullcm72@users.noreply.github.com>
2023-02-26 19:06:38 +0800

    Update EAIQ2.py

--- EAIQ2.py
+++ EAIQ2.py
@@ -1,14 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[29]:
-
-
 pip install SpeechRecognition pyphen
-
-
-# In[30]:
-
 
 import speech_recognition as sr
 import nltk
@@ -17,18 +10,11 @@ nltk.download('punkt')
 # Load the audio file
 audio_file = sr.AudioFile('ptk_2.wav')
 
-
-# In[31]:
-
-
 # Convert the audio file into text
 recognizer = sr.Recognizer()
 with audio_file as source:
     audio = recognizer.record(source)
 text = recognizer.recognize_google(audio)
-
-
-# In[32]:
 
 
 # Count the number of syllables in each word
